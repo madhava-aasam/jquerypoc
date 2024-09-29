1165a59551aff676350a58890d7ddf275ef9a30b
admin-aasam-at-851725622396 <madha@MADHAV-PC>
2024-09-28 23:57:57 -0400

    Add project files.

---
+++ Controllers/SampleController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+//using System.Web.Mvc;
+
+namespace jquerypoc.Controllers
+{
+    public class SampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetMessage()
+        {
+            var message = new { Text = "Hello from sample page" };
+            return new JsonResult(message);
+        }
+    }
+}
